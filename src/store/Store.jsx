ab01a5e22297f8c2245fbc447e3bda349805dbca
nils <nils@gangshi>
2023-02-13 16:07:29 -0500

    added new actions/action-types. moving away from modal/trigger elements towards selectors

--- src/store/Store.jsx
+++ src/store/Store.jsx
@@ -1,30 +1,39 @@
 // src/store/Store.js
 import React, { createContext, useReducer, useContext } from 'react';
 import PropTypes from 'prop-types';
+import chroma from 'chroma-js';
+
+import useSelector from 'Hooks/useSelector';
+import noop from 'Utility/noop';
 
 const initialState = {
+  onColorChange: noop,
   color: { h: 0, s: 0, v: 0 },
   isActive: false,
   isModalDragging: false,
-  modalElement: null,
-  triggerElement: null,
 };
 
 export const StoreContext = createContext();
 export const useStoreContext = () => useContext(StoreContext);
 export const DispatchContext = createContext();
-export const useDispatch = () => useContext(DispatchContext);
+export const useDispatch = () =>
+  useSelector(DispatchContext, (dispatch) => dispatch);
 
+const SET_ON_COLOR_CHANGE = 'SET_ON_COLOR_CHANGE';
 const SET_COLOR = 'SET_COLOR';
 const SET_HUE = 'SET_HUE';
 const SET_SATURATION = 'SET_SATURATION';
 const SET_VALUE = 'SET_VALUE';
+const SET_SATURATION_AND_VALUE = 'SET_SATURATION_AND_VALUE';
 const SET_IS_ACTIVE = 'SET_IS_ACTIVE';
 const SET_IS_MODAL_DRAGGING = 'SET_IS_MODAL_DRAGGING';
-const SET_MODAL_ELEMENT = 'SET_MODAL_ELEMENT';
-const SET_TRIGGER_ELEMENT = 'SET_TRIGGER_ELEMENT';
 
-export const setColor = (h, s, v) => ({
+export const setOnColorChange = (onColorChange) => ({
+  type: SET_ON_COLOR_CHANGE,
+  data: onColorChange,
+});
+
+export const setColor = ({ h, s, v }) => ({
   type: SET_COLOR,
   data: { color: { h, s, v } },
 });
@@ -37,6 +46,11 @@ export const setHue = (h) => ({
 export const setSaturation = (s) => ({
   type: SET_SATURATION,
   data: { s },
+});
+
+export const setSaturationAndValue = ({ s, v }) => ({
+  type: SET_SATURATION_AND_VALUE,
+  data: { s, v },
 });
 
 export const setValue = (v) => ({
@@ -54,58 +68,68 @@ export const setIsModalDragging = (isModalDragging) => ({
   data: { isModalDragging },
 });
 
-export const setModalElement = (modalElement) => ({
-  type: SET_MODAL_ELEMENT,
-  data: { modalElement },
-});
-
-export const setTriggerElement = (triggerElement) => ({
-  type: SET_TRIGGER_ELEMENT,
-  data: { triggerElement },
-});
-
+// @TODO: onColorChange needs to be added to state on mount (and if it changes)
+// then call it for SET_COLOR, SET_HUE, SET_SATURATION, SET_VALUE cases
 const reducer = (state, action) => {
   const { data, type } = action;
 
   switch (type) {
-    case SET_COLOR:
+    case SET_ON_COLOR_CHANGE:
       return {
         ...state,
-        color: {
-          ...(state.color || {}),
-          ...Object.entries(data.color || {}).reduce(
-            (components, [component, value]) =>
-              value != null && !Number.isNaN(value)
-                ? { ...components, [component]: value }
-                : components,
-            {}
-          ),
-        },
+        onColorChange: data,
       };
-    case SET_HUE:
+    case SET_COLOR: {
+      const { onColorChange } = state;
+      onColorChange(chroma(data.color));
       return {
         ...state,
-        color: {
-          ...state.color,
-          h: data.h,
-        },
+        color: data.color,
       };
-    case SET_SATURATION:
+    }
+    case SET_HUE: {
+      const color = { ...state.color, h: data.h };
+      const { onColorChange } = state;
+
+      onColorChange(chroma(color));
+
       return {
         ...state,
-        color: {
-          ...state.color,
-          s: data.s,
-        },
+        color,
       };
-    case SET_VALUE:
+    }
+    case SET_SATURATION: {
+      const color = { ...state.color, s: data.s };
+      const { onColorChange } = state;
+
+      onColorChange(chroma(color));
+
       return {
         ...state,
-        color: {
-          ...state.color,
-          v: data.v,
-        },
+        color,
       };
+    }
+    case SET_SATURATION_AND_VALUE: {
+      const color = { ...state.color, s: data.s, v: data.v };
+      const { onColorChange } = state;
+
+      onColorChange(chroma(color));
+
+      return {
+        ...state,
+        color,
+      };
+    }
+    case SET_VALUE: {
+      const color = { ...state.color, v: data.v };
+
+      state.onColorChange(color);
+
+      return {
+        ...state,
+        color,
+      };
+    }
     case SET_IS_ACTIVE:
       return {
         ...state,
@@ -116,16 +140,6 @@ const reducer = (state, action) => {
         ...state,
         isModalDragging: data.isModalDragging,
       };
-    case SET_MODAL_ELEMENT:
-      return {
-        ...state,
-        modalElement: data.modalElement,
-      };
-    case SET_TRIGGER_ELEMENT:
-      return {
-        ...state,
-        triggerElement: data.triggerElement,
-      };
     default:
       console.log(`undefined action: ${action.type}`, action);
       return state;
@@ -134,6 +148,8 @@ const reducer = (state, action) => {
 
 const Store = ({ children }) => {
   const [state, dispatch] = useReducer(reducer, initialState);
+
+  // @TODO: This is weird. Get rid of the DispatchContextProvider.
   return (
     <DispatchContext.Provider value={dispatch}>
       <StoreContext.Provider value={{ ...state, dispatch }}>
